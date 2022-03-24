5e5ea5fbac51739c3f62f2c775796ac89507f614
Bruno Souto <bcovies@gmail.com>
2022-03-24 15:37:06 -0300

    Adicionado script de tr

--- devops/test.sh
+++ devops/test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "DELETANDO A BRANCH $1"
+BRANCH=$1
+
+BRANCH_REGEX="^(feat|bug|bugfix|hotfix|chore)[_-][a-zA-Z_-]*[0-9]{3,4}"
+
+if [[ $BRANCH =~ $BRANCH_REGEX ]];
+then
+    export MRPI_NUMBER=$(echo "$BRANCH" | tr -dc '0-9')
+    echo "number=${MRPI_NUMBER}" >> $GITHUB_ENV
+else
+    echo "The branch ${BRANCH} was SUCCESSFULLY deleted."
+fi

ebcd520a01309f3370495ac2e2e893dcd703ec36
Bruno Souto <bcovies@gmail.com>
2022-03-24 15:42:22 -0300

    Adicionado

--- devops/test.sh
+++ devops/test.sh
@@ -3,7 +3,7 @@
 BRANCH=$1
 
 BRANCH_REGEX="^(feat|bug|bugfix|hotfix|chore)[_-][a-zA-Z_-]*[0-9]{3,4}"
-
+export MRPI_NUMBER=$(echo "$BRANCH" | tr -dc '0-9')
 echo "number=${MRPI_NUMBER}" 
 echo "The branch ${BRANCH} was SUCCESSFULLY deleted."
 

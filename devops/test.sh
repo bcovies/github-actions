7cfbd0ca105718b69de4f7d660370c196bda39c0
Bruno Souto <bcovies@gmail.com>
2022-03-24 15:40:08 -0300

    Adicionado

--- devops/test.sh
+++ devops/test.sh
@@ -4,10 +4,13 @@ BRANCH=$1
 
 BRANCH_REGEX="^(feat|bug|bugfix|hotfix|chore)[_-][a-zA-Z_-]*[0-9]{3,4}"
 
-if [[ $BRANCH =~ $BRANCH_REGEX ]];
-then
-    export MRPI_NUMBER=$(echo "$BRANCH" | tr -dc '0-9')
-    echo "number=${MRPI_NUMBER}" >> $GITHUB_ENV
-else
-    echo "The branch ${BRANCH} was SUCCESSFULLY deleted."
-fi
+echo "number=${MRPI_NUMBER}" >> $GITHUB_ENV
+echo "The branch ${BRANCH} was SUCCESSFULLY deleted."
+
+# if [[ $BRANCH =~ $BRANCH_REGEX ]];
+# then
+#     export MRPI_NUMBER=$(echo "$BRANCH" | tr -dc '0-9')
+#     echo "number=${MRPI_NUMBER}" >> $GITHUB_ENV
+# else
+#     echo "The branch ${BRANCH} was SUCCESSFULLY deleted."
+# fi
